07e977a5f8e294f3fee4e6a68acddf01b9ff7542
JayasimhaS2002 <sjayasimha05@gmail.com>
2024-06-22 20:51:38 +0530

    one c

--- joblisting/src/main/java/com/project/joblisting/repository/PostRepository.java
+++ joblisting/src/main/java/com/project/joblisting/repository/PostRepository.java
@@ -1,9 +1,9 @@
 package com.project.joblisting.repository;
 //two
 import org.springframework.data.mongodb.repository.MongoRepository;
-//two
+//one
 import com.project.joblisting.model.Post;
-//two
+//one
 public interface PostRepository extends MongoRepository<Post,String>// it is provided inbuilt in spring using which we can perform crud operations
 {
     
