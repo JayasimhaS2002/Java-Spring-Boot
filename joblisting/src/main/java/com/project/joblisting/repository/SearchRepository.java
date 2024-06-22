197e3dfa16c1b080567bc7b57a13d6a22707a4b1
JayasimhaS2002 <sjayasimha05@gmail.com>
2024-06-22 18:59:32 +0530

    changes to two

--- joblisting/src/main/java/com/project/joblisting/repository/SearchRepository.java
+++ joblisting/src/main/java/com/project/joblisting/repository/SearchRepository.java
@@ -1,9 +1,9 @@
 package com.project.joblisting.repository;
-
+//tw0
 import java.util.List;
-
+//two
 import com.project.joblisting.model.Post;
-
+//twos
 public interface SearchRepository {
 
     List<Post> findBytext(String text);
