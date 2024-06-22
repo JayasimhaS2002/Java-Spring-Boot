31161fc1603bd9e0b52c3a499752e6f2b00a8e24
JayasimhaS2002 <sjayasimha05@gmail.com>
2024-06-22 18:46:44 +0530

    one

--- joblisting/src/main/java/com/project/joblisting/JoblistingApplication.java
+++ joblisting/src/main/java/com/project/joblisting/JoblistingApplication.java
@@ -1,5 +1,5 @@
 package com.project.joblisting;
-
+//hellow 123 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
